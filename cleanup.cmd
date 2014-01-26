3924f69b22ebbde7622fcd9165d98eea3a31f973
thomasfischl <fischl.thomas@gmail.com>
2014-01-26 18:31:20 +0100

    Improve build process
    remove generated files

---
+++ cleanup.cmd
@@ -0,0 +1,10 @@
+del MiniCpp.exe
+del MiniCppLex.cs
+del MiniCppSem.cs
+del MiniCppSyn.cs
+
+del testProgramSwitch.emit.exe
+del testProgramSwitch.il
+del testProgramSwitch.text.exe
+del testProgramSwitch.text.il
+del testProgramSwitch_Gen.mcpp
--- compile.cmd
+++ compile.cmd
@@ -1,9 +1,7 @@
 SG minicpp.atg
 PGT minicpp.atg
-
 csc /debug *.cs 
 
-MiniCpp.exe testProgram.mcpp
-
-testProgram.emit.exe
-testProgram.text.exe
+MiniCpp.exe testProgramSwitch.mcpp
+testProgramSwitch.emit.exe
+testProgramSwitch.text.exe
